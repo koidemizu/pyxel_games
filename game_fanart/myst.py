754e129dc90602998ff22a106a319cbd4468ee2c
Taiki Koshiba <adjmtttkk@gmail.com>
2021-03-04 14:10:01 +0900

    Fifst commit.

---
+++ game_fanart/myst.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#myst.py
+
+
+import pyxel
+
+class APP:
+  def __init__(self):
+      pyxel.init(128, 128, caption="MYST")
+      #
+      pyxel.load('assets/myst.pyxres')
+      
+      pyxel.mouse(False)
+      
+      pyxel.run(self.update, self.draw)
+     
+  def update(self):
+      pass
+
+  def draw(self):
+      pyxel.cls(0)
+      #Draw tilemap
+      pyxel.bltm(0,0,0,32,0,16,16)
+
+                  
+APP()
