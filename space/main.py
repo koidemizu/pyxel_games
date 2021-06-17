f44dbbdb8c90c9b0b7844559b579d414c6625819
Taiki Koshiba <adjmtttkk@gmail.com>
2021-06-17 13:01:38 +0900

    Updated physics logic.

--- space/main.py
+++ space/main.py
@@ -6,8 +6,8 @@ class APP:
   def __init__(self):
       self.player = Player()
       
-      pyxel.init(128, 128, caption="TEST", fps=30)
-      #
+      pyxel.init(128, 128, caption="TEST", fps=30, scale=5,)
+      
       pyxel.load('assets/assets.pyxres')
       
       pyxel.mouse(False)
@@ -15,31 +15,34 @@ class APP:
       pyxel.run(self.update, self.draw)
      
   def update(self):
+      #Jump
       if self.player.p_j == True:
           j = self.check_move(1)
           self.player.Jump(j)    
+      #Fall
       else:
           d = self.check_move(3)
           if d == 3:
               self.player.Dawn()
-          
+              
+      #Move UP
       if pyxel.btn(pyxel.KEY_SPACE) and self.player.p_j == False:
           self.player.p_m = 1
           self.player.p_c += 1
           self.player.p_j = True
-
+      #Move RIGHT
       elif pyxel.btn(pyxel.KEY_RIGHT):
           self.player.p_m = 2
           self.player.p_c += 1
           a = self.check_move(2)
-          if self.player.p_x < 120 and a == 2:
+          if a == 2:
               self.player.p_x += 1
-              
+      #Move LEFT       
       elif pyxel.btn(pyxel.KEY_LEFT):
           self.player.p_m = 4
           self.player.p_c += 1
           a = self.check_move(4)
-          if self.player.p_x > 1 and a == 4:
+          if a == 4:
               self.player.p_x -= 1
 
       else:
@@ -53,6 +56,7 @@ class APP:
       #Draw tilemap
       pyxel.bltm(0,0,0,0,0,16,16)
       
+      #Pdayer draw//////////////////////////////
       x = self.player.p_x
       y = self.player.p_y
       if self.player.p_j == True:
@@ -75,33 +79,60 @@ class APP:
               pyxel.blt(x,y,0,8,16,8,8,0)
       elif self.player.p_m == 0:
           pyxel.blt(x,y,0,0,0,8,8,0)
+       #/////////////////////////////////////////
           
   def check_move(self, x):
-      px = int((self.player.p_x) / 8)
+      """
+      Player coordinates
+     
+      px--------------px2
+     px4|         px3|
+        |            |
+        |            |
+        |py4      py3|
+      py--------------py2
+
+      
+      """
+      px = int((self.player.p_x - 1) / 8)
       py = int((self.player.p_y) / 8)
       px2 = int((self.player.p_x + 8) / 8)
       py2 = int((self.player.p_y + 8) / 8)
-      px3 = int((self.player.p_x + 4) / 8)
-      #py3 = int((self.player.p_y + 4) / 8)
-      pm = pyxel.tilemap(0).get(px, py)
-      pm2 = pyxel.tilemap(0).get(px2, py)
-      pm3 = pyxel.tilemap(0).get(px, py2)
-      pm4 = pyxel.tilemap(0).get(px2, py2)
-      pm5 = pyxel.tilemap(0).get(px3, py2)
+      px3 = int((self.player.p_x + 6) / 8)
+      px4 = int((self.player.p_x) / 8)
+      py3 = int((self.player.p_y + 6) / 8)
+      py4 = int((self.player.p_y + 2) / 8)
+      
+      pm = pyxel.tilemap(0).get(px3, py)
+      pm2 = pyxel.tilemap(0).get(px4, py)
+      pm3 = pyxel.tilemap(0).get(px3, py2)
+      pm4 = pyxel.tilemap(0).get(px4, py2)
+      
+      pm5 = pyxel.tilemap(0).get(px, py3)
+      pm6 = pyxel.tilemap(0).get(px2, py3)
+      pm7 = pyxel.tilemap(0).get(px, py4)
+      pm8 = pyxel.tilemap(0).get(px2, py4)
+      
+      #Move UP
       if x == 1:
           if pm > 50 and pm2 > 50:
               return 1
-      elif x == 2:
-          if pm5 > 50 or pm2 > 50 or pm4 > 50:
-              return 2
+
+      #Move DOWN
       elif x == 3:
-          if pm3 > 50 or  pm4 > 50:
-              if pm5 < 50:
-                  return 0
-              else:
-                  return 3
+          if pm3 > 50 and  pm4 > 50:
+              return 3
+          else:
+              return 0
+            
+      #Move RIGHT
+      elif x == 2:
+          if pm6 > 50 and pm8 > 50:
+              return 2
+          
+      #Move LEFT
       elif x == 4:
-          if pm5 > 50 or pm3 > 50 or pm > 50:
+          if pm5 > 50 and pm7 > 50:
               return 4
       else:
           return 0
@@ -115,14 +146,17 @@ class Player:
         self.p_c = 0
         self.p_j = False
         self.p_jc = 0
+        
     def Jump(self, x):
-        if self.p_y > 5 and x == 1:
+        if x == 1:
             self.p_y -= 1.5
         self.p_jc += 1
+        
         if self.p_jc == 10 :
             self.p_j = False
             self.p_jc = 0
     def Dawn(self):
+        
         self.p_y += 1
 
                   
