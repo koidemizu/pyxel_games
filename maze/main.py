7aa70f9ca6dc5d1d69c1417fe09154ff15f9dcdc
Taiki Koshiba <adjmtttkk@gmail.com>
2022-01-07 10:44:59 +0900

    床へのマッピング機能、エフェクト描画を追加

--- maze/main.py
+++ maze/main.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import pyxel
+from random import randint, choice
 
 class APP:
   def __init__(self):
       pyxel.init(256, 256, title = "pyxel")
       pyxel.load('assets/assets.pyxres')
-      
+             
+      #Maze set
       self.data = []
       self.maze = []
-       
-      #Maze set
+      
       for i in range(32):
           self.data = []
           for i2 in range(32):
@@ -19,6 +20,8 @@ class APP:
       
       #Player position
       self.pos = [1, 1]
+      #Move permission tile list      
+      self.move_permit = [(0, 0), (0, 1)]
       
       #Angle P=Player
       #    ^
@@ -29,44 +32,93 @@ class APP:
       #     
       self.pos_angle = 3
       
+      #Wall set
       self.wall = [[0, 0],
                    [0, 0],
                    [0, 0],
                    [0, 0],]
-      #Wall set
+      
       for i1 in range(5):
           x1 = self.pos[1] - (1 * i1)
           if self.maze[x1][self.pos[0]] == 1:
               self.dead_end = i1      
       self.dead_end = 0
       
+      self.wall_list = [(1, 0), (1, 1), (1, 2)]
+      
+      #Floor
+      self.floor = [0, 0, 0, 0]
+      
+      #Bubble
+      self.bubbles = [] 
+      #Bubble color list
+      self.bub_c = (1, 5, 6, 12)
+      
+      
       pyxel.run(self.update, self.draw)
      
   def update(self): 
+      #Bubble generate test---------------------------------------------------
+      if pyxel.btnp(pyxel.KEY_B):
+          c = choice(self.bub_c)
+          x = randint(0, 256)
+          y = randint(160, 220)
+          v = randint(0, 2)
+          s1 = randint(1, 2)
+          s2 = randint(1, 9) / 10
+          s = s1 + s2
+          new_bubble = Bubble(x, y, v, c, s)
+          self.bubbles.append(new_bubble)
+      #-----------------------------------------------------------------------
+      
+      #New bubble create------------------------------------------------------
+      if pyxel.frame_count % 300 == 0:
+          b_num = randint(2, 10)
+          for bn in range(b_num):
+              c = choice(self.bub_c)
+              x = randint(30, 220)
+              y = randint(160, 220)
+              v = randint(0, 2)
+              s1 = randint(1, 2)
+              s2 = randint(1, 9) / 10
+              s = s1 + s2
+              new_bubble = Bubble(x, y, v, c, s)
+              self.bubbles.append(new_bubble)
+              
+      #Bubble update----------------------------------------------------------    
+      for b in self.bubbles:
+          b.update()
+          if b.bub_y < 0:
+              del b              
+       #----------------------------------------------------------------------
+          
       
       #Wall reset
       self.wall = [[0, 0],
                    [0, 0],
                    [0, 0],
                    [0, 0],]      
+      self.floor = [0, 0, 0, 0]
               
+      #Player Controll--------------------------------------------------------
       #Go ahead
       if pyxel.btnp(pyxel.KEY_UP):
+ 
           self.dead_end = 0
           if self.pos_angle == 1:
-              if self.maze[self.pos[1] - 1][self.pos[0]] == (0, 0):
+              if self.maze[self.pos[1] - 1][self.pos[0]] in self.move_permit:
                   self.pos[1] = self.pos[1] - 1
                   self.pos_angle = 1
           elif self.pos_angle == 2:
-              if self.maze[self.pos[1]][self.pos[0] + 1] == (0, 0):
+              if self.maze[self.pos[1]][self.pos[0] + 1] in self.move_permit:
                   self.pos[0] = self.pos[0] + 1
                   self.pos_angle = 2
           elif self.pos_angle == 3:
-              if self.maze[self.pos[1] + 1][self.pos[0]] == (0, 0):
+              if self.maze[self.pos[1] + 1][self.pos[0]] in self.move_permit:
                   self.pos[1] = self.pos[1] + 1
                   self.pos_angle = 3
           elif self.pos_angle == 4:
-              if self.maze[self.pos[1]][self.pos[0] - 1] == (0, 0):
+              if self.maze[self.pos[1]][self.pos[0] - 1] in self.move_permit:
                   self.pos[0] = self.pos[0] - 1     
                   self.pos_angle = 4
                   
@@ -106,24 +158,28 @@ class APP:
               self.pos_angle = 2
           elif self.pos_angle == 4:
               self.pos_angle = 3    
+      #-----------------------------------------------------------------------
   
-      #Wall set///////////////////////////////////////////////////////////////        
+      #Wall-Floor set---------------------------------------------------------      
       if self.pos_angle == 1:
           for i1 in range(4):
               if self.pos[1] - i1 < 0:
-                  break
+                  break           
               for i2 in range(2):
                   p1 = self.pos[1] - (1 * i1)
                   p2 = self.pos[0] - 1 + (2 * i2) 
               
                   self.wall[i1][i2] = self.maze[p1][p2]
-              
-                  if self.maze[p1][self.pos[0]] == (1, 0):
+                               
+                  if self.maze[p1][self.pos[0]] in self.wall_list:
                       if self.dead_end == 0:
                           self.dead_end = i1
                       if self.dead_end > i1:
                           self.dead_end = i1
-                      
+                  elif self.maze[p1][self.pos[0]] == (0, 1):
+                      self.floor[i1] = 1
+
+                        
       elif self.pos_angle == 3:
           for i1 in range(4):
               if self.pos[1] + i1 > 31:
@@ -134,11 +190,13 @@ class APP:
               
                   self.wall[i1][i2] = self.maze[p1][p2]
               
-                  if self.maze[p1][self.pos[0]] == (1, 0):
+                  if self.maze[p1][self.pos[0]] in self.wall_list:
                       if self.dead_end == 0:
                           self.dead_end = i1
                       if self.dead_end > i1:
                           self.dead_end = i1
+                  elif self.maze[p1][self.pos[0]] == (0, 1):
+                      self.floor[i1] = 1                          
                       
       elif self.pos_angle == 2:
           for i1 in range(4):
@@ -150,11 +208,13 @@ class APP:
               
                   self.wall[i1][i2] = self.maze[p1][p2]
               
-                  if self.maze[self.pos[1]][p2] == (1, 0):
+                  if self.maze[self.pos[1]][p2] in self.wall_list:
                       if self.dead_end == 0:
                           self.dead_end = i1
                       if self.dead_end > i1:
                           self.dead_end = i1             
+                  elif self.maze[self.pos[1]][p2] == (0, 1):
+                      self.floor[i1] = 1                          
                           
       elif self.pos_angle == 4:
           for i1 in range(4):
@@ -166,42 +226,22 @@ class APP:
               
                   self.wall[i1][i2] = self.maze[p1][p2]
               
-                  if self.maze[self.pos[1]][p2] == (1, 0):
+                  if self.maze[self.pos[1]][p2] in self.wall_list:
                       if self.dead_end == 0:
                           self.dead_end = i1
                       if self.dead_end > i1:
-                          self.dead_end = i1            
-      #///////////////////////////////////////////////////////////////////////
+                          self.dead_end = i1        
+                  elif self.maze[self.pos[1]][p2] == (0, 1):
+                      self.floor[i1] = 1                          
+      #-----------------------------------------------------------------------
+      
+      #Paint floor action-----------------------------------------------------
+      if pyxel.btnp(pyxel.KEY_SPACE):
+          self.maze[self.pos[1]][self.pos[0]] = (0, 1)                        
+      #-----------------------------------------------------------------------
 
   def draw(self):
-      pyxel.cls(0)
-      
-#Bubble draw test--------------------------------------------------------------      
-      #bx = 120
-      #by = 80
-     
-      #pyxel.circb(bx, by, 10, 6)
-      #pyxel.circb(bx - 5, by - 5, 3, 6)
-      #pyxel.circb(bx - 6, by - 6, 1, 7)
-      
-      #bx = 120
-      #by = 110
-     
-      #pyxel.circb(bx, by, 7, 6)
-      #pyxel.circb(bx - 3, by - 3, 2, 6)
-      #pyxel.circb(bx - 4, by - 4, 1, 7)      
-      
-      
-      #bx = 120
-      #by = 130
-     
-      #pyxel.circb(bx, by, 5, 6)
-      #pyxel.circb(bx - 3, by - 1, 1, 6)
-      #pyxel.circb(bx - 4, by - 2, 1, 7)      
-      
-#------------------------------------------------------------------------------      
-      
-      pyxel.rectb(0, 150, 256, 106, 1)
+      pyxel.cls(0)      
       
 #Draw wall 1------------------------------------------------------------------      
       if self.wall[0][0] == (1, 0):
@@ -273,6 +313,22 @@ class APP:
          pyxel.line(148, 60, 148, 90, 1)      
 #-----------------------------------------------------------------------------      
     
+#Draw Floor paint-------------------------------------------------------------
+      if self.floor[0] == 1:
+          pyxel.line(67, 145, 178, 130, 8)
+          pyxel.line(70, 130, 178, 145, 8)
+      if self.floor[1] == 1:
+          pyxel.line(95, 117, 155, 107, 8)
+          pyxel.line(95, 107, 155, 117, 8)
+      if self.floor[2] == 1:
+          pyxel.line(110, 104, 140, 97, 1)
+          pyxel.line(110, 97, 140, 104, 1)        
+      if self.floor[3] == 1:
+          pyxel.line(115, 92, 135, 95, 1)
+          pyxel.line(115, 95, 135, 92, 1)    
+      
+#-----------------------------------------------------------------------------        
+
 #Draw Dead-End----------------------------------------------------------------
       if self.dead_end == 0:
           pass
@@ -288,46 +344,98 @@ class APP:
       elif self.dead_end == 4:
           pyxel.rect(108, 60, 41, 30, 0)
           pyxel.rectb(108, 60, 41, 30, 1)
-#-----------------------------------------------------------------------------          
+#-----------------------------------------------------------------------------     
+
           
+      
+#Bubble draw test--------------------------------------------------------------     
+      for b in self.bubbles :
+          bx = b.bub_x
+          by = b.bub_y
+          bc = b.bub_c
+          bv = b.bub_v
+          
+          if bv == 0:
+              pyxel.circb(bx, by, 10, bc)
+              pyxel.circb(bx - 5, by - 5, 3, bc)
+              pyxel.circb(bx - 6, by - 6, 1, 7)
+          elif bv == 1:     
+              pyxel.circb(bx, by, 7, 6)
+              pyxel.circb(bx - 3, by - 3, 2, bc)
+              pyxel.circb(bx - 4, by - 4, 1, bc)      
+          elif bv == 2:
+              pyxel.circb(bx, by, 5, 6)
+              pyxel.circb(bx - 3, by - 1, 1, bc)
+              pyxel.circb(bx - 4, by - 2, 1, bc)      
+      
+#------------------------------------------------------------------------------      
+
 #Draw Compass-----------------------------------------------------------------
-      pyxel.rectb(14, 164, 55, 45, 13)
+      pyxel.rect(0, 150, 256, 106, 0)
+      pyxel.rectb(0, 150, 256, 106, 1)
+      pyxel.rectb(14, 164, 55, 45, 13)   
+        
       for d0 in range(5):
           if (self.pos[1]-1 < 0 or 
               self.pos[0]-2+d0 < 0 or self.pos[0]-2+d0 > 31):
-              pass          
-          elif self.maze[self.pos[1]-1][self.pos[0]-2+d0] == (1, 0):
               pyxel.rect(17+10*d0, 167, 9, 9, 7)
+          elif self.maze[self.pos[1]-1][self.pos[0]-2+d0] in self.wall_list:
+              pyxel.rect(17+10*d0, 167, 9, 9, 7)
+          elif self.maze[self.pos[1]-1][self.pos[0]-2+d0] == (2, 0):
+              pyxel.rect(17+10*d0, 167, 9, 9, 3)              
+          elif self.maze[self.pos[1]-1][self.pos[0]-2+d0] == (0, 1):
+              pyxel.rectb(17+10*d0, 167, 9, 9, 8)              
       for d1 in range(5):
           if self.pos[0]-2+d1 < 0 or self.pos[0]-2+d1 > 31:
-              pass          
-          elif self.maze[self.pos[1]][self.pos[0]-2+d1] == (1, 0):
               pyxel.rect(17+10*d1, 177, 9, 9, 7)
+          elif self.maze[self.pos[1]][self.pos[0]-2+d1] in self.wall_list:
+              pyxel.rect(17+10*d1, 177, 9, 9, 7)
+          elif self.maze[self.pos[1]][self.pos[0]-2+d1] == (2, 0):
+              pyxel.rect(17+10*d1, 177, 9, 9, 3)              
+          elif self.maze[self.pos[1]][self.pos[0]-2+d1] == (0, 1):
+              pyxel.rectb(17+10*d1, 177, 9, 9, 8)              
       for d2 in range(5):
           if (self.pos[1]+1 > 31 or
               self.pos[0]-2+d2 < 0 or self.pos[0]-2+d2 > 31):
-              pass          
-          elif self.maze[self.pos[1]+1][self.pos[0]-2+d2] == (1, 0):
               pyxel.rect(17+10*d2, 187, 9, 9, 7)
+          elif self.maze[self.pos[1]+1][self.pos[0]-2+d2] in self.wall_list:
+              pyxel.rect(17+10*d2, 187, 9, 9, 7)
+          elif self.maze[self.pos[1]+1][self.pos[0]-2+d2] == (2, 0):
+              pyxel.rect(17+10*d2, 187, 9, 9, 3)              
+          elif self.maze[self.pos[1]+1][self.pos[0]-2+d2] == (0, 1):
+              pyxel.rectb(17+10*d2, 187, 9, 9, 8)              
       for d3 in range(5):
           if (self.pos[1]+2 > 31 or
               self.pos[0]-2+d3 < 0 or self.pos[0]-2+d3 > 31):
-              pass          
-          elif self.maze[self.pos[1]+2][self.pos[0]-2+d3] == (1, 0):
+              pyxel.rect(17+10*d3, 197, 9, 9, 7)      
+          elif self.maze[self.pos[1]+2][self.pos[0]-2+d3] in self.wall_list:
               pyxel.rect(17+10*d3, 197, 9, 9, 7)              
+          elif self.maze[self.pos[1]+2][self.pos[0]-2+d3] == (2, 0):
+              pyxel.rect(17+10*d3, 197, 9, 9, 3)                            
+          elif self.maze[self.pos[1]+2][self.pos[0]-2+d3] == (0, 1):
+              pyxel.rectb(17+10*d3, 197, 9, 9, 8)                            
 
             
       if self.pos_angle == 1:
-          pyxel.tri(41, 178, 37, 185, 45, 185, 8)
+          pyxel.tri(41, 178, 37, 185, 45, 185, 1)
       elif self.pos_angle == 2:
-          pyxel.tri(45, 181, 37, 178, 37, 185, 8)
+          pyxel.tri(45, 181, 37, 178, 37, 185, 1)
       elif self.pos_angle == 3:
-          pyxel.tri(41, 185, 37, 178, 45, 178, 8)
+          pyxel.tri(41, 185, 37, 178, 45, 178, 1)
       elif self.pos_angle == 4:
-          pyxel.tri(37, 183, 45, 178, 45, 186, 8)
+          pyxel.tri(37, 183, 45, 178, 45, 186, 1)
 #-----------------------------------------------------------------------------      
-      
-      
+
+class Bubble:
+  def __init__(self, x, y, v, c, s):
+      self.bub_x = x
+      self.bub_y = y
+      self.bub_v = v
+      self.bub_c = c      
+      self.bub_s = s
+  def update(self):
+      self.bub_y -= self.bub_s
+  
       
 APP()
 
